3012f741cd36d4e2437b78b8afcc3198ec247432
Julien Jerphanion <git@jjerphan.xyz>
2024-03-28 10:42:04 +0100

    Support osx_arm64
    Signed-off-by: Julien Jerphanion <git@jjerphan.xyz>

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+if [[ "$CONDA_BUILD_CROSS_COMPILATION" == "1" ]]; then
+  # Get an updated config.sub and config.guess
+  # See: https://conda-forge.org/docs/maintainer/knowledge_base.html#cross-compilation
+  cp $BUILD_PREFIX/share/gnuconfig/config.* .
+fi
+
 cmake ${CMAKE_ARGS} -DBUILD_TESTS=OFF \
       -DCMAKE_INSTALL_PREFIX=$PREFIX \
       -DCMAKE_INSTALL_LIBDIR=lib \
